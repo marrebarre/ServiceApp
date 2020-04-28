1f3621d9fff1bf9ba39a35f2e38dcf47440fa627
Robbanyowakim <Robertyowakim@gmail.com>
2020-04-28 11:38:58 +0200

    Test

--- ServiceApp/src/sample/Main.java
+++ ServiceApp/src/sample/Main.java
@@ -20,6 +20,7 @@ public class Main extends Application {
         primaryStage.setFullScreen(true);
         this.stage = primaryStage;
         this.stage.show();
+        System.out.println("Hej");
     }
 
 
