ccc831bea405843a699e646a6ba329db1618c99b
Robbanyowakim <Robertyowakim@gmail.com>
2020-05-08 10:56:48 +0200

    Modified worker GUI

--- ServiceApp/src/sample/worker/Worker.java
+++ ServiceApp/src/sample/worker/Worker.java
@@ -1,4 +1,19 @@
 package sample.worker;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import sample.Main;
+
 public class Worker {
+
+    @FXML
+    void editProfile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logout(ActionEvent event) throws Exception {
+        Main.switcher.fsMode = Main.stage.isFullScreen();
+        Main.switcher.scene(Main.stage,"login/login.fxml");
+    }
 }
